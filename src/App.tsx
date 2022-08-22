3cb13f8e1598210106221f6ed8fad90ced440705
Illia Vodonis <illavodonis@gmail.com>
2022-08-22 19:19:10 +0300

    todo App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { TodoApp } from './components/TodoApp';
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/">
+        <Route index element={<TodoApp />} />
+        <Route path="active" element={<TodoApp />} />
+        <Route path="completed" element={<TodoApp />} />
+      </Route>
+    </Routes>
   );
-}
-
-export default App;
+};
